9c6219a80b2e7f8cf69343b13b42943d0657bcba
Duy Nguyen <hoanduy27@gmail.com>
2021-05-31 16:56:35 +0700

    update iam

--- iam.py
+++ iam.py
@@ -1,4 +1,4 @@
-from ldap3 import Server, Connection, ALL, SUBTREE
+from ldap3 import ALL_ATTRIBUTES, Server, Connection, ALL, SUBTREE
 import ldap3
 from ldap3.core.exceptions import LDAPException, LDAPBindError
 from ldap3.core.tls import Tls
@@ -7,7 +7,7 @@ import yaml
 import ssl
 
 class IAM:
-    def __init__(self):
+    def __init__(self, username, password, role):
         with open('config.yml', 'r') as f:
             config = yaml.safe_load(f)
         
@@ -15,20 +15,52 @@ class IAM:
         self.search_base = config['searchBase']
         tls_configuration = Tls(validate=ssl.CERT_REQUIRED, version=ssl.PROTOCOL_TLSv1_2)
         self.server = Server(self.server_uri, use_ssl=True, tls=tls_configuration)
+        self.username = username
+        self.password = password
+        self.role = role
+        self.conn = self.authenticate()
+
+    def addUser(self, username, password):
+        if(self.role=='admin'):
+            #TODO
+            pass
+    
+    def removeUser(self, username, password):
+        if(self.role=='admin'):
+            #TODO
+            pass
+    
+    def changeRole(self, username, role):
+        if(self.role == 'admin'):
+            #TODO
+            pass
         
-    def authentication(self, method, username, password, role):
+    def authenticate(self):
         conn = ldap3.Connection(\
             self.server, 
-            user=f'uid={username},ou={role},{self.search_base}', 
-            password=password
-        )        
+            user=f'uid={self.username},ou=user,{self.search_base}', 
+            password=self.password
+        ) if self.role != 'admin' \
+        else ldap3.Connection(
+            self.server,
+            user = f'uid={self.username},{self.search_base}',
+            password=self.password
+        )
+        
         if conn.bind():
             print("Logged in")
         else:
             print("Wrong password or usn")
             conn.unbind()
-
+        
+        
         return conn
+    
+    def changePassword(self, password):
+        pass
 
-app = IAM()
-app.authentication(username="duynguyen", password="duynguyen13")
+app = IAM('duynguyen', 'duynguyen123', 'developer')
+
+app.conn.search(search_base=app.conn.user, search_filter=f'(objectClass=*)', attributes=['gidNumber'])
+print(app.conn.entries[0])
+
